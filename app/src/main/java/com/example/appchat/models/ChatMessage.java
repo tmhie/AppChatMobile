049a98c566b427ba06d171287af59924b750d4e2
DaoQuocHuy <75303365+DaoQuocHuy@users.noreply.github.com>
2022-12-07 09:09:51 +0700

    update v2

--- app/src/main/java/com/example/appchat/models/ChatMessage.java
+++ app/src/main/java/com/example/appchat/models/ChatMessage.java
@@ -1,5 +1,8 @@
 package com.example.appchat.models;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId, receiverId, message, dateTime;
+    public Date dateObject;
 }
